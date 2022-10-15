6a8e3d36d41f0174a7d1f3ce8e6b0b6bb2025852
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-10-16 00:29:54 +0100

    ➕ Adds full installation script and packages listing

---
+++ packages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export REPO=(
+  ansible
+  ansible-lint
+  autorandr
+  ctags
+  curl
+  diff-so-fancy
+  diffpdf
+  direnv
+  htop
+  jq
+  fzf
+  neofetch
+  maven
+  pv
+  ranger
+  shfmt
+  tree
+  unzip
+  vagrant
+  wget
+)
+
+export AUR=(
+  direnv
+  emojify
+  insect
+  neovim
+  rainbow
+  zeal
+)
