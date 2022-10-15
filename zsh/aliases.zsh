e6ae79e9576c680fe4cad35a6dd15a9ff41baa62
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-10-15 23:50:57 +0100

    ➕ Adds zsh configuration support

---
+++ zsh/aliases.zsh
@@ -0,0 +1,59 @@
+# GLOBAL ALIASES
+
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+
+# ALIASES
+
+alias dot="cd $DOTFILES"
+
+alias c="coding $HOME/Code"
+alias s="coding $HOME/Snippets"
+
+alias r="ranger"
+alias ka="killall"
+alias :q="exit"
+
+alias o=open
+
+alias bsc=backup
+alias pls=please
+
+alias g-='cd $(git rev-parse --show-toplevel || echo ".")'
+alias grw=git-review
+alias ght=git-history
+alias gfz=git-fuzzy
+
+alias v="nvim -p"
+alias vim="nvim"
+
+alias py="ipython -i"
+alias pm="python -m"
+
+alias t=tmux
+alias tc=tmux-connect
+alias mux=tmuxinator
+
+alias bs="bin/server"
+alias bb="bin/build"
+alias br="bin/run"
+alias bt="bin/test"
+alias bf="bin/format"
+alias bl="bin/lint"
+alias bcs="bin/console"
+alias bcl="bin/clean"
+alias bst="bin/setup"
+alias bsr="bin/start"
+alias bsp="bin/stop"
+
+alias mk=make
+alias mkh="make help"
+alias mkc="make clean"
+alias mkr="make run"
+alias mkt="make test"
+
+alias a="ansible"
+alias ac="ansible-console"
+alias ap="ansible-playbook"
---
+++ zsh/theme.zsh
@@ -0,0 +1,7 @@
+# THEME
+export ZSH=~/.oh-my-zsh
+export TERM="xterm-256color"
+
+ZSH_THEME="eastwood"
+
+source $ZSH/oh-my-zsh.sh
