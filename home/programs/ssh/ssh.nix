b19771316b0d549512d6875f8642776e3997fa3a
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2026-07-16 13:10:43 +0100

    chore: tailor ssh setup to my needs

--- home/programs/ssh/ssh.nix
+++ home/programs/ssh/ssh.nix
@@ -10,5 +10,13 @@
     openssh
   ];
 
-  home.file.".ssh/config" = mkSymlink "config";
+  home.file = {
+    ".ssh/config" = mkSymlink "config";
+    ".ssh/platform.ini" =
+      if pkgs.stdenv.hostPlatform.isDarwin then mkSymlink "platforms/macos"
+      else mkSymlink "platforms/linux";
+    ".ssh/hosts/thinkpad.ini" = mkSymlink "hosts/thinkpad.ini";
+    ".ssh/profiles/personal.ini" = mkSymlink "profiles/personal.ini";
+    ".ssh/profiles/nutrium.ini" = mkSymlink "profiles/nutrium.ini";
+  };
 }
