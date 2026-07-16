d0445da203e996fded3e80d394acbda0e6a57a99
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2026-07-16 12:04:07 +0100

    chore: change boot file to my hostname

--- system/modules/nixos/boot/boot.nix
+++ system/modules/nixos/boot/boot.nix
@@ -9,15 +9,9 @@ lib.mkMerge [
   {
     boot.plymouth.enable = true;
   }
-  (onHost "framework" {
+  (onHost "thinkpad" {
     boot.loader.systemd-boot.enable = true;
     boot.loader.systemd-boot.configurationLimit = 10;
     boot.loader.efi.canTouchEfiVariables = true;
-  })
-  (onHost "temis" {
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda";
-    boot.loader.grub.useOSProber = true;
-    boot.loader.grub.configurationLimit = 10;
   })
 ]
